8da07b10e7c6d1de4e2d4ca41947551dd6f23c70
Ian Kjos <kjosib@gmail.com>
2023-11-19 21:28:30 -0600

    delete some old cruft

--- sophie/adapters/teletype_adapter.py
+++ sophie/adapters/teletype_adapter.py
@@ -1,34 +1,10 @@
 import sys
 import random
-from ..runtime import iterate_list, force, Message
-from ..scheduler import NativeObjectProxy, MAIN_QUEUE
+from ..runtime import iterate_list, Message
+from ..scheduler import NativeObjectProxy
 
 NIL : dict
 
-def sophie_init():
-	return {
-		'done':run_app,
-		'echo':run_app,
-		'read':run_app,
-		'random':run_app,
-	}
-
-def run_app(env, app):
-	while True:
-		tag = app[""]
-		if tag == 'done': return
-		elif tag == 'echo':
-			for fragment in iterate_list(app['text']):
-				sys.stdout.write(fragment)
-			sys.stdout.flush()
-			app = force(app['next'])
-		elif tag == 'read':
-			proc = force(app['next'])
-			app = force(proc.apply([input()]))
-		elif tag == 'random':
-			proc = force(app['next'])
-			app = force(proc.apply([random.random()]))
-	
 class Console:
 	@staticmethod
 	def echo(text):
