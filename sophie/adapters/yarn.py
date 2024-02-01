d04574ffeae85a7a094fde0e4663d72186ae9b53
Ian Kjos <kjosib@gmail.com>
2024-02-01 03:19:07 -0600

    Repair a test case

--- sophie/adapters/yarn.py
+++ sophie/adapters/yarn.py
@@ -13,3 +13,7 @@ def val(aString):
 	except ValueError: return nope
 	else: return {"":"this", "item":answer}
 
+def identity(x):
+	# This one's just for messing around with the type system.
+	# Something in the zoo-of-ok imports it across the FFI.
+	return x
